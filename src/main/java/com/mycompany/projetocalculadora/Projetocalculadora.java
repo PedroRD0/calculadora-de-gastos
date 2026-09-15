b86ab109bc2b3973c3b4ec271e497127a665bcca
Pedro Duarte <pedroluisduarte2307@gmail.com>
2026-09-15 12:16:10 -0300

    Adiciona entrada de dados e calculo de gastos

--- src/main/java/com/mycompany/projetocalculadora/Projetocalculadora.java
+++ src/main/java/com/mycompany/projetocalculadora/Projetocalculadora.java
@@ -4,25 +4,34 @@
 
 package com.mycompany.projetocalculadora;
 
+import java.util.Scanner;
 /**
  *
- * @author PICHAU
+ * @author Pedro
  */
 public class Projetocalculadora {
 
     public static void main(String[] args){
-   
-        String nome = "Pedro";
         
-        float salario = 3600f;
+        Scanner teclado = new Scanner(System.in);
         
-        float aluguel = 790f;
+        System.out.print("Digite seu nome: ");
+        String nome = teclado.nextLine();
         
-        float alimentacao = 650f;
+        System.out.print("Digite seu salario: ");
+        float salario = teclado.nextFloat();
+         
+        System.out.print("Digite seu aluguel: ");
+        float aluguel = teclado.nextFloat();
         
-        float estudo = 350f;
+        System.out.print("Digite seu gasto com alimentacao: ");
+        float alimentacao = teclado.nextFloat();
         
-        float lazer = 600f;
+        System.out.print("Digite seu gasto com estudo: ");
+        float estudo = teclado.nextFloat();
+        
+        System.out.print("Digite seu gasto com lazer: ");
+        float lazer = teclado.nextFloat();
               
         float totalGastos = aluguel + alimentacao + estudo + lazer;
         
@@ -30,8 +39,15 @@ public class Projetocalculadora {
         
         System.out.printf("olá %s \n", nome);
         
+         System.out.printf("seu salario: %.2f \n", salario);
+        
+        System.out.printf("seu gasto foi de: %.2f \n", totalGastos);
+        /*neste printf é para entregar o saldo gasto para melhor controle do 
+        usuario
+        */
         System.out.printf("seu saldo é: %.2f \n", saldo);
         /* aqui vai aparecer somente o saldo somado
         */
+        
                 }
 }
